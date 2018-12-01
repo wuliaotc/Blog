e806babc2f7928896556d92e94e5975bfc179d55
wuliaotc <372072553@qq.com>
2018-12-01 21:35:04 +0800

    display more information of Article in admin page

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import User,Article
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=['Title','Author','Launch_Time','Edit_Time']
 
-admin.site.register([User,Article])
+
+admin.site.register(User)
+admin.site.register(Article,ArticleAdmin)
