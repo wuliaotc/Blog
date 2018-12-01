aac5ef684c51a56ccdd307789a13b04979c9ddd3
wuliaotc <372072553@qq.com>
2018-12-01 22:58:24 +0800

    make login page available

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from .models import User
 # Create your views here.
 hobby={'read','sport','game','music'}
 
@@ -13,18 +14,43 @@ def register(request):
         return render(request,'app/work_flex_part01.html')
 
     elif request.method=="POST":
-        UserName=request.POST['USN']
+        UserName=request.POST['USN']#不可重复 需要校验 空格等非法字符
         PassWord=request.POST['PWD']
-        Email=request.POST['Email']
+        Email=request.POST['Email']#不可重复
+         
+        pass#这里需要加上参数校验
+
+
+        #判断用户名是否被使用过
+        if User.objects.filter(UserName=UserName).exists():
+            return HttpResponse("此用户名已被使用")
+        if User.objects.filter(Email=Email).exists():
+            return HttpResponse("此邮箱已被绑定")
         
-        return HttpResponse(UserName+'<br>'+PassWord+'<br>'+Email)
+        User.objects.create(UserName=UserName,Email=Email,PassWord=PassWord)
+        
+        return HttpResponse("注册成功")
         #return HttpResponseRedirect(request.session['home']) 
 
         
 def login(request):
-    return HttpResponse('咕咕咕')
-    
+    if request.method=="GET":
+        return render(request,'app/login.html')
 
+    elif request.method=="POST":
+        UserName=request.POST['USN']#不可重复 需要校验 空格等非法字符
+        PassWord=request.POST['PWD']
+
+
+        temp=User.objects.filter(UserName=UserName)
+        if not temp.exists():
+            return HttpResponse("此用户名不存在")
+        elif temp[0].PassWord !=PassWord:
+            return HttpResponse("密码错误")
+        else:
+            return HttpResponse("登陆成功")
+
+        #return HttpResponseRedirect(request.session['home']) 
 
 def setting(request):
     if request.method=='GET':
