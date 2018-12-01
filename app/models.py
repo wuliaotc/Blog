32caea01ef63b25e6979c5199c198f2d7bc68394
wuliaotc <372072553@qq.com>
2018-12-01 22:00:30 +0800

    make User.PassWord unrepeatable

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
     #用户id,用户的唯一身份验证,不可重复
     UserID = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
-    #用户名称 可重复
+    #用户名称 不可重复
     UserName = models.CharField(max_length=USERNAME_LENGTH)
     #用户邮箱地址 用于验证?
     Email = models.EmailField()
